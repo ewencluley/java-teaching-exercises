8c6d5779d5e13671b3315537d04dc8e29efbcd8c
Bwatch Macbook <bladimoracher@gmail.com>
2021-04-01 00:58:53 -0400

    Fininshing up the exaggerated result (X2) for the provided string.

--- 01-http-requests/src/main/java/com/ewencluley/javatasks/httprequests/HandlerPostRqst.java
+++ 01-http-requests/src/main/java/com/ewencluley/javatasks/httprequests/HandlerPostRqst.java
@@ -4,20 +4,46 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class HandlerPostRqst implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
-        String request = exchange.getRequestMethod();
-        InputStream exchangeRequestBody = exchange.getRequestBody();
-        byte[] exchangedData = exchangeRequestBody.readAllBytes();
-        String exchangedDateToString = new String(exchangedData, StandardCharsets.UTF_8);
+        // Getting all the exchanged bytes and passing it to a string on the following line
+        byte[] exchangedData = exchange.getRequestBody().readAllBytes();
+        String[] exchangedDataToStringLines = new String(exchangedData, StandardCharsets.UTF_8).split("\n");
 
+        StringBuilder exchangedDataToString = new StringBuilder();
 
-        System.out.println(exchangedDateToString);
+        // get the numbers from each line,
+        // multiply the resulting number by 2
+        for (String s : exchangedDataToStringLines) {
+            String number = s.replaceAll("\\s*\\D+\\s*","");
+            if (!number.isEmpty()) {
+                exchangedDataToString.append(s.replace(number, String.valueOf(Integer.parseInt(number) * 2))).append("\n");
+            } else {
+                exchangedDataToString.append(s).append("\n");
+            }
+        }
+        // filter out non post methods
+        if (exchange.getRequestMethod().equals("POST")) {
+        responseData(exchange, exchangedDataToString.toString(), 200);
+        } else {
+            responseData(exchange, "Method Not allowed", 405);
+        }
+    }
 
+    public void responseData(HttpExchange exchange, String responseMessage, int responseCode) throws IOException{
+
+        // send response and size of message to the client
+        byte[] responseMessageBytes = responseMessage.getBytes(StandardCharsets.UTF_8);
+        exchange.sendResponseHeaders(responseCode, responseMessage.getBytes(StandardCharsets.UTF_8).length);
+
+        //Get the body to send the message, write the message to on the body and close connection/request
+        OutputStream responseBody = exchange.getResponseBody();
+        responseBody.write(responseMessageBytes);
+        responseBody.close();
     }
 }
